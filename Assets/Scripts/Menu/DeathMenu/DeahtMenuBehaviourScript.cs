c3f446c969fa9a7e8e199303c81131c3a4f39269
Samuel Rohr <samuel.scherrer@gmail.com>
2014-12-01 21:16:08 -0200

    Entrega final

---
+++ Assets/Scripts/Menu/DeathMenu/DeahtMenuBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeahtMenuBehaviourScript : MenuBehaviour {
+
+	void Start () {
+		// Devemos colocar o nome do jogador perdedor
+		TextMesh textMesh = transform.FindChild ("Texts").FindChild ("defeatedPlayerText").GetComponent<TextMesh> ();
+		textMesh.text = ApplicationData.defeatedPlayerName + " lost";
+	}
+	
+	public override void TreatClickEvent (string buttonName, Transform button) {
+		switch (buttonName) {
+		case "retryButton":
+			Application.LoadLevel("Match");
+			break;
+		case "mainMenuButton":
+			Application.LoadLevel ("MainMenu");
+			break;
+		}
+	}
+
+}
